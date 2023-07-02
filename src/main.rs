2d4372ce8d4a92bf5c14856b6b749dbd73912550
IssaAboudi <ma42202@hotmail.com>
2023-07-01 23:41:03 -0700

    Cursor bound checking & Page Up/Down Handling
    	Prevent cursor from going off the screen.
    
    	Handle pressing page up and page down to jump around the screen

--- src/main.rs
+++ src/main.rs
@@ -42,25 +42,32 @@ macro_rules! PRFX {
 
 macro_rules! ARROW_UP {
     () => {
-        b'w'
+        1000
     };
 }
-
 macro_rules! ARROW_DOWN {
     () => {
-        b's'
+        1001
     };
 }
-
 macro_rules! ARROW_LEFT {
     () => {
-        b'a'
+        1002
     };
 }
-
 macro_rules! ARROW_RIGHT {
     () => {
-        b'd'
+        1003
+    };
+}
+macro_rules! PAGE_UP {
+    () => {
+        1004
+    };
+}
+macro_rules! PAGE_DOWN {
+    () => {
+        1005
     };
 }
 
@@ -183,7 +190,7 @@ impl Terminal {
                     };
                     let parsed_cols = match parts[0].trim().parse::<c_int>() {
                         Ok(t) => t,
-                        Err(e) => {
+                        Err(_e) => {
                             return Err(Error::new(
                                 Other,
                                 "Invalid parsing: parsed_cols in getCursorPosition",
@@ -235,7 +242,7 @@ impl Drop for Terminal {
 }
 
 // process input
-fn editorReadKey() -> io::Result<u8> {
+fn editorReadKey() -> io::Result<i32> {
     let mut c = [0u8; 1];
     loop {
         //read 1 byte in
@@ -260,35 +267,67 @@ fn editorReadKey() -> io::Result<u8> {
         //if escape follows with a [
         // then it's an escape sequence
         if seq[0] == '[' as u8 {
-            //translate into arrow keys
-            return match seq[1] {
-                b'A' => Ok(ARROW_UP!()),
-                b'B' => Ok(ARROW_DOWN!()),
-                b'C' => Ok(ARROW_RIGHT!()),
-                b'D' => Ok(ARROW_LEFT!()),
-                _ => Ok(b'\x1b'),
-            };
+            if seq[1] >= b'0' && seq[1] <= b'9' {
+                //translate page up and page down
+                if seq[2] == b'~' {
+                    return match seq[1] {
+                        b'5' => Ok(PAGE_UP!()),
+                        b'6' => Ok(PAGE_DOWN!()),
+                        _ => Ok(b'\x1b' as i32),
+                    };
+                }
+            } else {
+                //translate arrow keys
+                return match seq[1] {
+                    b'A' => Ok(ARROW_UP!()),
+                    b'B' => Ok(ARROW_DOWN!()),
+                    b'C' => Ok(ARROW_RIGHT!()),
+                    b'D' => Ok(ARROW_LEFT!()),
+                    _ => Ok(b'\x1b' as i32),
+                };
+            }
         }
 
-        Ok(b'\x1b')
+        Ok(b'\x1b' as i32)
     } else {
-        Ok(c[0])
+        Ok(c[0] as i32)
     }
 }
 
 fn editorProcessKeypress(terminal: &mut Terminal) -> io::Result<bool> {
     match editorReadKey() {
         Ok(c) => {
-            if c == CTRL_KEY!('q' as u8) {
+            if c == CTRL_KEY!('q' as u8) as i32 {
                 Ok(true)
             } else {
+                //trigger page up and page down
+                if c == PAGE_UP!() || c == PAGE_DOWN!() {
+                    let mut times = terminal.screen_rows;
+                    while times > 0 {
+                        match editorMoveCursor(
+                            terminal,
+                            if c == PAGE_UP!() {
+                                ARROW_UP!()
+                            } else {
+                                ARROW_DOWN!()
+                            },
+                        ) {
+                            Ok(_t) => {
+                                times -= 1;
+                            }
+                            Err(e) => return Err(Error::new(Other, e)),
+                        };
+                    }
+                }
+
+                //trigger cursor movement
                 if c == ARROW_UP!()
                     || c == ARROW_DOWN!()
                     || c == ARROW_LEFT!()
                     || c == ARROW_RIGHT!()
                 {
                     return match editorMoveCursor(terminal, c) {
-                        Ok(t) => Ok(false),
+                        Ok(_t) => Ok(false),
                         Err(e) => Err(Error::new(Other, e)),
                     };
                 };
@@ -299,12 +338,30 @@ fn editorProcessKeypress(terminal: &mut Terminal) -> io::Result<bool> {
     }
 }
 
-fn editorMoveCursor(terminal: &mut Terminal, key: u8) -> io::Result<()> {
+fn editorMoveCursor(terminal: &mut Terminal, key: i32) -> io::Result<()> {
+    //movement with bounds checking
     match key {
-        ARROW_LEFT!() => terminal.curs_x -= 1,
-        ARROW_RIGHT!() => terminal.curs_x += 1,
-        ARROW_UP!() => terminal.curs_y -= 1,
-        ARROW_DOWN!() => terminal.curs_y += 1,
+        ARROW_LEFT!() => {
+            if terminal.curs_x > 0 {
+                terminal.curs_x -= 1
+            }
+        }
+        ARROW_RIGHT!() => {
+            if terminal.curs_x < terminal.screen_cols - 1 {
+                terminal.curs_x += 1
+            }
+        }
+        ARROW_UP!() => {
+            if terminal.curs_y > 0 {
+                terminal.curs_y -= 1
+            }
+        }
+        ARROW_DOWN!() => {
+            if terminal.curs_y < terminal.screen_rows - 1 {
+                terminal.curs_y += 1
+            }
+        }
+        //to catch any keys that slip past
         _ => return Err(Error::new(Other, "Invalid key in editorMoveCursor")),
     }
     Ok(())
