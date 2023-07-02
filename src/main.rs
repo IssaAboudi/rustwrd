22ba6341f0314b87ba214de558572dad25ee65a7
IssaAboudi <ma42202@hotmail.com>
2023-07-01 21:40:46 -0700

    Added cursor movement with arrow keys
    	Overlap between WASD and arrow keys. We'll need to adjust the
    mapping to be out of range of the typical keypresses. But for now we're
    able to move around the window with the arrow keys.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,67 @@
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
 
-use std::ffi::c_int;
-use std::fs::read;
-use std::io;
-use std::io::{Error, ErrorKind, Read, stdin, stdout, Write};
-use std::io::ErrorKind::Other;
-use std::mem::size_of;
-use std::ops::Add;
-use std::os::fd::AsRawFd;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use nix::errno::errno;
-use nix::libc::{c_ushort, EAGAIN, exit, ioctl, ISTRIP, perror, STDIN_FILENO, STDOUT_FILENO, TIOCGWINSZ, winsize};
+use nix::libc::{
+    c_ushort, exit, ioctl, perror, winsize, EAGAIN, ISTRIP, STDIN_FILENO, STDOUT_FILENO, TIOCGWINSZ,
+};
 use nix::sys::termios;
 use nix::sys::termios::SpecialCharacterIndices::{VMIN, VTIME};
 use nix::unistd::acct::disable;
+use std::ffi::c_int;
+use std::fs::read;
+use std::io;
+use std::io::ErrorKind::Other;
+use std::io::{stdin, stdout, Error, ErrorKind, Read, Write};
+use std::mem::size_of;
+use std::ops::Add;
+use std::os::fd::AsRawFd;
 
 //Macro to add CTRL modifier to each key
 macro_rules! CTRL_KEY {
     ($k : expr) => {
         $k & 0x1f
-    }
+    };
 }
 
 //Version of our editor
-
 macro_rules! RUST_WRD {
-    () => {"0.0.1"}
+    () => {
+        "0.0.1"
+    };
+}
+
+//Line Prefix - what's at the beginning of every line in the editor
+macro_rules! PRFX {
+    () => {
+        b"."
+    };
+}
+
+// editorKey bindings:
+
+macro_rules! ARROW_UP {
+    () => {
+        b'w'
+    };
+}
+
+macro_rules! ARROW_DOWN {
+    () => {
+        b's'
+    };
+}
+
+macro_rules! ARROW_LEFT {
+    () => {
+        b'a'
+    };
+}
+
+macro_rules! ARROW_RIGHT {
+    () => {
+        b'd'
+    };
 }
 
 struct Terminal {
@@ -38,28 +73,31 @@ struct Terminal {
 }
 
 impl Terminal {
-
     fn enableRawMode(&mut self) -> io::Result<()> {
         let fd = stdin().as_raw_fd(); //file descriptor for raw stdin
         self.orig_termios = termios::tcgetattr(fd).unwrap();
 
         let mut raw = self.orig_termios.clone();
 
-        raw.input_flags.remove(termios::InputFlags::IXON // disable sw control flow comamnds
+        raw.input_flags.remove(
+            termios::InputFlags::IXON // disable sw control flow comamnds
             | termios::InputFlags::ICRNL
             //Other flags
             | termios::InputFlags::BRKINT //break condition causes a SIGINT signal (^c)
             | termios::InputFlags::INPCK // enables parity checking
-            | termios::InputFlags::ISTRIP // 8th bit of each input byte to be stripped
+            | termios::InputFlags::ISTRIP, // 8th bit of each input byte to be stripped
         );
-        raw.output_flags.remove(termios::OutputFlags::OPOST // disable '\n' -> '\r\n' translation
+        raw.output_flags.remove(
+            termios::OutputFlags::OPOST, // disable '\n' -> '\r\n' translation
         );
-        raw.control_flags.remove( termios::ControlFlags::CS8 // sets character size to 8 bits per byte
+        raw.control_flags.remove(
+            termios::ControlFlags::CS8, // sets character size to 8 bits per byte
         );
-        raw.local_flags.remove(termios::LocalFlags::ECHO //disables your input being spit back to you
+        raw.local_flags.remove(
+            termios::LocalFlags::ECHO //disables your input being spit back to you
             | termios::LocalFlags::ICANON //disables cannonical mode
             | termios::LocalFlags::ISIG // disables interrupts (^c, ^z)
-            | termios::LocalFlags::IEXTEN // disables typing other characters literally (^v)
+            | termios::LocalFlags::IEXTEN, // disables typing other characters literally (^v)
         );
 
         raw.control_chars[VMIN as usize] = 0; //return as soon as there is any input to be read
@@ -83,9 +121,7 @@ impl Terminal {
             // we tell terminal to move to bottom right edge with large values
             match stdout().write_all(b"\x1b[999C\x1b[999B") {
                 Ok(_c) => {}
-                Err(_e) => {
-                    return Err(Error::new(Other, "Error: Failed write at getWindowSize"))
-                }
+                Err(_e) => return Err(Error::new(Other, "Error: Failed write at getWindowSize")),
             }
             return self.getCursorPosition(rows, cols);
         } else {
@@ -93,7 +129,6 @@ impl Terminal {
             *cols = ws.ws_col as c_int;
             Ok(())
         }
-
     }
 
     fn getCursorPosition(&self, rows: &mut c_int, cols: &mut c_int) -> io::Result<()> {
@@ -105,7 +140,7 @@ impl Terminal {
 
                 let mut i = 0;
 
-                let mut c = [0u8;1];
+                let mut c = [0u8; 1];
                 let mut nread;
                 loop {
                     //loop through buffer
@@ -127,44 +162,50 @@ impl Terminal {
 
                 //If invalid, error out
                 if buf[0] != '\x1b' || buf[1] != '[' {
-                    return Err(Error::new(Other, "Invalid escape sequence at getCursorPosition"));
+                    return Err(Error::new(
+                        Other,
+                        "Invalid escape sequence at getCursorPosition",
+                    ));
                 }
 
                 //parse the buffer ignoring the first byte: \x1b
-                let input : String = buf[2..].iter().collect();
+                let input: String = buf[2..].iter().collect();
                 let parts: Vec<&str> = input.split(";").collect();
                 if parts.len() == 2 {
                     let parsed_rows = match parts[0].trim().parse::<c_int>() {
                         Ok(t) => t,
-                        Err(e) => {
-                            return Err(Error::new(Other, "Invalid parsing: parsed_rows in getCursorPosition"));
+                        Err(_e) => {
+                            return Err(Error::new(
+                                Other,
+                                "Invalid parsing: parsed_rows in getCursorPosition",
+                            ));
                         }
                     };
                     let parsed_cols = match parts[0].trim().parse::<c_int>() {
                         Ok(t) => t,
                         Err(e) => {
-                            return Err(Error::new(Other, "Invalid parsing: parsed_cols in getCursorPosition"));
+                            return Err(Error::new(
+                                Other,
+                                "Invalid parsing: parsed_cols in getCursorPosition",
+                            ));
                         }
                     };
 
                     *rows = parsed_rows;
                     *cols = parsed_cols;
-
                 } else {
-                    return Err(Error::new(Other, "Invalid parsing of parts in getCursorPosition"))
+                    return Err(Error::new(
+                        Other,
+                        "Invalid parsing of parts in getCursorPosition",
+                    ));
                 }
-
 
                 match editorReadKey() {
                     Ok(_t) => {}
-                    Err(e) => {
-                        return Err(Error::new(Other, e))
-                    }
+                    Err(e) => return Err(Error::new(Other, e)),
                 }
             }
-            Err(_e) => {
-                return Err(Error::new(Other, "bad write at getCursorPosition"))
-            }
+            Err(_e) => return Err(Error::new(Other, "bad write at getCursorPosition")),
         };
         Ok(())
     }
@@ -181,11 +222,8 @@ impl Terminal {
                 self.screen_cols = cols;
                 Ok(())
             }
-            Err(e) => {
-                return Err(Error::new(Other, e))
-            }
+            Err(e) => return Err(Error::new(Other, e)),
         }
-
     }
 }
 
@@ -196,46 +234,85 @@ impl Drop for Terminal {
     }
 }
 
-// get the pressed keys
-fn editorReadKey() -> io::Result<u8>{
+// process input
+fn editorReadKey() -> io::Result<u8> {
     let mut c = [0u8; 1];
     loop {
+        //read 1 byte in
         match stdin().read(&mut c) {
             Ok(t) => {
                 if t == 1 {
+                    //print the byte we read in
                     print!("{}", c[0] as char);
                     break;
                 }
             }
-            Err(e) => {
-                return Err(Error::new(Other, e))
-            }
+            Err(e) => return Err(Error::new(Other, e)),
         };
     }
-    Ok(c[0])
+
+    // if key pressed was escape sequence beginning
+    if c[0] == b'\x1b' {
+        let mut seq = [0u8; 3];
+        //read the next bytes
+        stdin().read(&mut seq)?;
+
+        //if escape follows with a [
+        // then it's an escape sequence
+        if seq[0] == '[' as u8 {
+            //translate into arrow keys
+            return match seq[1] {
+                b'A' => Ok(ARROW_UP!()),
+                b'B' => Ok(ARROW_DOWN!()),
+                b'C' => Ok(ARROW_RIGHT!()),
+                b'D' => Ok(ARROW_LEFT!()),
+                _ => Ok(b'\x1b'),
+            };
+        }
+
+        Ok(b'\x1b')
+    } else {
+        Ok(c[0])
+    }
 }
 
-// process input
-fn editorProcessKeypress() -> io::Result<bool>{
+fn editorProcessKeypress(terminal: &mut Terminal) -> io::Result<bool> {
     match editorReadKey() {
         Ok(c) => {
-            if c == CTRL_KEY!('q' as u8){
+            if c == CTRL_KEY!('q' as u8) {
                 Ok(true)
             } else {
+                if c == ARROW_UP!()
+                    || c == ARROW_DOWN!()
+                    || c == ARROW_LEFT!()
+                    || c == ARROW_RIGHT!()
+                {
+                    return match editorMoveCursor(terminal, c) {
+                        Ok(t) => Ok(false),
+                        Err(e) => Err(Error::new(Other, e)),
+                    };
+                };
                 Ok(false)
             }
         }
-        Err(_e) => {
-            Err(Error::new(Other, "failed at editorReadKey"))
-        }
+        Err(_e) => Err(Error::new(Other, "failed at editorReadKey")),
     }
+}
 
+fn editorMoveCursor(terminal: &mut Terminal, key: u8) -> io::Result<()> {
+    match key {
+        ARROW_LEFT!() => terminal.curs_x -= 1,
+        ARROW_RIGHT!() => terminal.curs_x += 1,
+        ARROW_UP!() => terminal.curs_y -= 1,
+        ARROW_DOWN!() => terminal.curs_y += 1,
+        _ => return Err(Error::new(Other, "Invalid key in editorMoveCursor")),
+    }
+    Ok(())
 }
 
 // write out
 fn editorRefreshScreen(terminal: &Terminal) -> io::Result<()> {
-
-    let mut appendBuf : Vec<u8> = Vec::new();
+    let mut appendBuf: Vec<u8> = Vec::new();
 
     //add escape sequences to buffer and build up a batch of stuff to do
     // as opposed to small writes
@@ -243,10 +320,11 @@ fn editorRefreshScreen(terminal: &Terminal) -> io::Result<()> {
     appendBuf.extend(b"\x1b[H");
 
     editorDrawRows(terminal, &mut appendBuf)?;
+
     let curs_x = terminal.curs_x + 1;
     let curs_y = terminal.curs_y + 1;
 
-    let buf = "\x1b[".to_string() + &curs_y.to_string() + ";" + &curs_x.to_string() + "H";
+    let buf = format!("\x1b[{};{}H", curs_y, curs_x);
     appendBuf.extend(buf.as_bytes());
 
     appendBuf.extend(b"\x1b[?25h");
@@ -257,14 +335,15 @@ fn editorRefreshScreen(terminal: &Terminal) -> io::Result<()> {
     Ok(())
 }
 
-// draw rows
-fn editorDrawRows(terminal : &Terminal, ab : &mut Vec<u8>) -> io::Result<()> {
+fn editorDrawRows(terminal: &Terminal, ab: &mut Vec<u8>) -> io::Result<()> {
     let mut i = 0;
     loop {
-        if i > terminal.screen_rows { break; }
+        if i > terminal.screen_rows - 2 {
+            break;
+        }
 
         //add a new line for every line but the last one
-        if i < terminal.screen_rows + 1  {
+        if i < terminal.screen_rows - 1 {
             ab.extend(b"\r\n");
         }
 
@@ -277,10 +356,10 @@ fn editorDrawRows(terminal : &Terminal, ab : &mut Vec<u8>) -> io::Result<()> {
             let author = "by Issa Aboudi 2023";
 
             //center welcome message
-            let padding = ( terminal.screen_cols - welcome.len() as i32 ) / 2;
+            let padding = (terminal.screen_cols - welcome.len() as i32) / 2;
             if padding > 0 {
-                ab.extend(b".");
-                let spaces=" ".repeat(padding as usize);
+                ab.extend(PRFX!());
+                let spaces = " ".repeat(padding as usize);
                 ab.extend(spaces.as_bytes());
             }
 
@@ -288,17 +367,16 @@ fn editorDrawRows(terminal : &Terminal, ab : &mut Vec<u8>) -> io::Result<()> {
             ab.extend(RUST_WRD!().as_bytes()); // add version number macro
             ab.extend(b"\r\n");
 
-            let padding = (( terminal.screen_cols - author.len() as i32) / 2 ) + 3;
+            let padding = ((terminal.screen_cols - author.len() as i32) / 2) + 3;
             if padding > 0 {
-                ab.extend(b".");
+                ab.extend(PRFX!());
                 let spaces = " ".repeat(padding as usize);
                 ab.extend(spaces.as_bytes());
             }
             ab.extend(author.as_bytes());
-
         } else {
             // write a period on every line
-            ab.extend(b".");
+            ab.extend(PRFX!());
         }
 
         i += 1;
@@ -307,13 +385,14 @@ fn editorDrawRows(terminal : &Terminal, ab : &mut Vec<u8>) -> io::Result<()> {
     Ok(())
 }
 
+// entry point
 fn main() -> io::Result<()> {
     let mut terminal = Terminal {
         orig_termios: termios::tcgetattr(STDIN_FILENO)?,
         screen_rows: 0,
         screen_cols: 0,
-        curs_x: 10,
-        curs_y: 50,
+        curs_x: 0,
+        curs_y: 0,
     };
 
     terminal.enableRawMode()?;
@@ -321,13 +400,14 @@ fn main() -> io::Result<()> {
 
     loop {
         editorRefreshScreen(&terminal)?;
-        match editorProcessKeypress() {
+        match editorProcessKeypress(&mut terminal) {
             Ok(exit) => {
                 if exit == true {
                     let _status = stdout().write_all(b"\x1b[2J")?;
                     let _status = stdout().write_all(b"\x1b[H")?;
                     break;
-                } else {}
+                } else {
+                }
             }
             Err(_e) => {
                 editorRefreshScreen(&terminal)?;
